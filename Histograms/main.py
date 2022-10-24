18fa8e338a219762c86add0c760875b02eea7544
Hasan Can Ozbek <hasancanozbekk@gmail.com>
2022-10-24 21:06:33 +0300

    Updated project Histogram

--- Histograms/main.py
+++ Histograms/main.py
@@ -2,17 +2,17 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as ml
 
-y_boyut = 500
-x_boyut = 500
-foto = cv2.resize(cv2.imread("photo.jpg", 0), (x_boyut, y_boyut))
+y_size = 500
+x_size = 500
+photo = cv2.resize(cv2.imread("photo.jpg", 0), (x_size, y_size))
 
-cv2.imshow("foto", foto)
+cv2.imshow("foto", photo)
 # cv2.waitKey()
 
 hist = np.zeros(256, dtype=int)
-for f in range(x_boyut):
-    for k in range(y_boyut):
-        hist[foto[f, k]] = hist[foto[f, k]] + 1
+for f in range(x_size):
+    for k in range(y_size):
+        hist[photo[f, k]] = hist[photo[f, k]] + 1
 
 ml.plot(hist)
 ml.show()
